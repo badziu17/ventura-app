0d4f9c5b011c09b4a0af9f3a32ceb2dd2af9aa06
bartek_zaczek <32414538+badziu17@users.noreply.github.com>
2026-02-21 20:03:35 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Ventura from './Ventura'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Ventura />
-  </React.StrictMode>,
-)
